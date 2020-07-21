1bafe9aecf7c2620dff1b23ecfa33c804e5db138
santosh <santospandey333@gmail.com>
2020-07-21 11:40:05 +0545

    before deploy heroku

---
+++ public/build/precache-manifest.06c4f3c342fc000b37045fa4391a626f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee6c0d14696259c4b79f3a59c1c3e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a0c67ae20c7fbebb4af",
+    "url": "/static/css/main.08f5b427.chunk.css"
+  },
+  {
+    "revision": "1ec9f444fda8c84160cd",
+    "url": "/static/js/2.d4e570db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4e570db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0c67ae20c7fbebb4af",
+    "url": "/static/js/main.a7850190.chunk.js"
+  },
+  {
+    "revision": "f07e1c499998113fece4",
+    "url": "/static/js/runtime-main.f6808e98.js"
+  }
+]);
